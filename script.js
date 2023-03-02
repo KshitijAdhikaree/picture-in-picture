2bde63319d8bf3a5411305d17fceeab47fe6ab5b
Kshitij adhikaree <adhikareekhsitij@gmail.com>
2023-03-02 18:42:47 +0545

    reset button

---
+++ script.js
@@ -0,0 +1,27 @@
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
+
+//Prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    // Catch error
+    console.log(error);
+  }
+}
+
+button.addEventListener("click", async () => {
+  //Disable button on click
+  button.disabled = true;
+});
+// Start pic in pic
+await videoElement.reqestPictureInPicture();
+//Reset Button
+button.disabled = false;
+//On load
+selectMediaStream();
